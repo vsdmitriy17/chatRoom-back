9267a1bac17dc89949553022b88a29b736532e5c
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2023-03-11 21:25:01 +0200

    feature/webSocket-disconnect

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,22 @@ app.use(cors); // подключаем корсы
 const serverHttp = http.createServer(app); // Сервер Прокси (HTTP сервер)
 
 const serverSocket = new Server(serverHttp, { // Создаем сервер сокетов
-    cors: { // добавляем корсы на сервер сокетов
-        origin: "*", // хосты с кот разрешено подключение, "*" - с любых
-        methods: ["GET","POST"], // методы запросов кот. разрешены
-    }
+  cors: { // добавляем корсы на сервер сокетов
+    origin: "*", // хосты с кот разрешено подключение, "*" - с любых
+      methods: ["GET","POST"], // методы запросов кот. разрешены
+  }
 });
 
-serverSocket.on("connection", () => { // вешаем слушателя события "connection" на сервер сокетов
-    console.log("WebSocket connection success!");
+serverSocket.on("connection", (serverIo) => { // вешаем слушателя события "connection" на сервер сокетов
+  console.log("WebSocket connection started!");
+
+  serverIo.on("disconnect", () => { // вешаем слушателя события "disconnect" на сервер сокетов
+    console.log("WebSocket connection closed!");
+  });
 });
 
 serverHttp.listen(PORT, () => {  // Запускаем Сервер Прокси (HTTP сервер) на порте 5000
-    console.log(`Server is runing on port ${PORT}`);
+  console.log(`Server is runing on port ${PORT}`);
 });
 
 
