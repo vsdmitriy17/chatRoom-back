6b5df71eeccd4de53743c09d7360bdcfaf688328
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2023-03-11 21:17:13 +0200

    feature/cors-config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
+import cors from 'cors';
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 
 const PORT = 5000;
 const app = express();
+app.use(cors); // подключаем корсы
+
 const serverHttp = http.createServer(app); // Сервер Прокси (HTTP сервер)
-const serverSocket = new Server(serverHttp); //Создаем сервер сокетов
+
+const serverSocket = new Server(serverHttp, { // Создаем сервер сокетов
+    cors: { // добавляем корсы на сервер сокетов
+        origin: "*", // хосты с кот разрешено подключение, "*" - с любых
+        methods: ["GET","POST"], // методы запросов кот. разрешены
+    }
+});
 
 serverSocket.on("connection", () => { // вешаем слушателя события "connection" на сервер сокетов
     console.log("WebSocket connection success!");
