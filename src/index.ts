10c6b52b42ced05f32d40c4787ae31e60be73031
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2023-03-11 19:50:55 +0200

    feature/serverHttp, serverSocket - run

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-console.log("Server works!!");
-const a = "str1";
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const PORT = 5000;
+const app = express();
+const serverHttp = http.createServer(app); // Сервер Прокси (HTTP сервер)
+const serverSocket = new Server(serverHttp); //Создаем сервер сокетов
+
+serverSocket.on("connection", () => { // вешаем слушателя события "connection" на сервер сокетов
+    console.log("WebSocket connection success!");
+});
+
+serverHttp.listen(PORT, () => {  // Запускаем Сервер Прокси (HTTP сервер) на порте 5000
+    console.log(`Server is runing on port ${PORT}`);
+});
+
+
