ebc8d7b42c05ff42958a63ed3512449b8bfd4438
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2023-03-13 07:27:20 +0200

    feature/leaveRoom

--- src/room/index.ts
+++ src/room/index.ts
@@ -1,15 +1,43 @@
 import { Socket } from 'socket.io';
 import { v4 as uuidV4 } from 'uuid';
 
+const rooms: Record<string, string[]> = {}; // массив каналов (перенести в БД)
+
+interface RoomParams { // тип для параметров joinRoom
+  roomId: string,
+  peerId: string,
+}
+
 export const roomHendler = (socketIo: Socket) => {
   const createRoom = () => {
     const roomId = uuidV4(); // Создаем уникальный айди канала
+    rooms[roomId] = [];
+
     socketIo.emit("room-created", { roomId }); // Отправляем юзеру (клиенту) сообщение о создании канала и его айди
     console.log("User created the room");
   }
-  const joinRoom = ({ roomId }: { roomId: string }) => {
-    socketIo.join(roomId); // Присоединяемся к каналу
-    console.log("User joined the room", roomId);
+
+  const joinRoom = ({ roomId, peerId }: RoomParams) => {
+    if (rooms[roomId]) {
+      rooms[roomId].push(peerId);
+
+      socketIo.join(roomId); // Присоединяемся к каналу
+      socketIo.emit("get-users", { 
+        roomId,
+        participants: rooms[roomId],
+      });
+      console.log("User joined the room", roomId, peerId);
+    }
+
+    socketIo.on("disconnect", () => {
+      console.log("User left the room", peerId);
+      leaveRoom({ roomId, peerId });
+    });
+  }
+
+  const leaveRoom = ({ roomId, peerId }: RoomParams) => {
+    rooms[roomId] = rooms[roomId].filter((id) => id !== peerId);
+    socketIo.to(roomId).emit("user-disconnected", peerId)
   }
 
   socketIo.on("create-room", createRoom); // вешаем слушателя события "create-room" на сервер сокетов
